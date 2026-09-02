8f2587b48eff980c2b1ea319560b5f9b94c098e2
Alejandro Alvarez <zorro201021@hotmail.com>
2026-09-02 16:48:08 -0300

    Correccion en base de datoss

--- actualizacion.sql
+++ actualizacion.sql
@@ -1,4 +1,5 @@
 update cliente set accesocredito = 'Confirmado' where estado like '%';
 
+ALTER TABLE local ADD COLUMN direccion VARCHAR(255), ADD COLUMN ciudad VARCHAR(100), ADD COLUMN telefono VARCHAR(100);
 INSERT INTO datos_empresa (nombre, ruc, telefono) VALUES ('Casa Maric', '4193548-9', '(0985 712 481)');
 UPDATE local SET direccion= '14 de Mayo y Joaquín Estigarribia', ciudad= 'Villarrica - Guaira', telefono= '(0985 712 481)' WHERE cod_local = 1;
